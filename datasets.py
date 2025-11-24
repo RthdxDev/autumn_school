2498e48c085db8083ec2869357c345efbe59a819
matta <mattarasov704@gmail.com>
2025-11-24 16:10:11 +0300

    change transformer, datasets for masking

--- datasets.py
+++ datasets.py
@@ -7,27 +7,37 @@ class NamesDataset(Dataset):
 	def __init__(self, names: list[str], seq_len: int=3):
 		self._seq_len = seq_len
 		self.__stoi = {
-			"<start>": 0,
-			"<end>": 1,
+			"<pad>": 0,
+			"<start>": 1,
+			"<end>": 2,	
 		}
+
 		for i, ch in enumerate(sorted(set(''.join(names)))):
-			self.__stoi[ch] = i + 2
+			self.__stoi[ch] = i + 3
 		self.__itos = {i: ch for ch, i in self.__stoi.items()}
-		self.names: list[tuple[list[str], str]] = []
+
+		self.names: list[tuple[str, str]] = []
 		self.names_encoded: list[tuple[Tensor, Tensor]] = []
+
 		for name in names:
-			name_lst = list(name) + ['<end>']
-			for i in range(len(name_lst)):
-				target: str = name_lst[i]
-				target_encoded = torch.tensor([self.__stoi[target]])
-				part_name = name_lst[max(0, i - self.seq_len):i]
-				entry = ["<start>"] * (self.seq_len - len(part_name)) + part_name
-				entry_encoded = torch.tensor([self.__stoi["<start>"]] * (self.seq_len - len(part_name)) + [self.__stoi[ch] for ch in part_name])
-				self.names.append((entry, target))
-				self.names_encoded.append((entry_encoded, target_encoded))
+			tokens = [self.__stoi['<start>']] + [self.__stoi[ch] for ch in name] + [self.__stoi['<end>']]
+			strs = ['<start>'] + list(name) + ['<end>']
+
+			if len(tokens) < seq_len:
+				tokens = tokens + [self.__stoi['<pad>']] * (seq_len - len(tokens))
+				strs = strs + ['<pad>'] * (seq_len - len(strs))
+			elif len(tokens) > seq_len:
+				tokens = tokens[:seq_len]
+				strs = strs[:seq_len]
+
+			input_seq = torch.tensor(tokens[:-1])
+			target_seq = torch.tensor(tokens[1:])
+
+			self.names.append((''.join(strs[:-1]), ''.join(strs[1:])))
+			self.names_encoded.append((input_seq, target_seq))
 
 	def __len__(self):
-		return len(self.names)
+		return len(self.names_encoded)
 	
 	@property
 	def seq_len(self):
